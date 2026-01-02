4f55e8e21bcefebf9f62cafa18471545b7fb3bf4
podcasting35-dev <podcasting35@gmail.com>
2026-01-02 15:13:06 +0530

    Delete menu.js

--- menu.js
+++
@@ -1,7 +0,0 @@
-
-import express from 'express';
-import Menu from '../models/Menu.js';
-const r=express.Router();
-r.get('/',async(req,res)=>res.json(await Menu.find()));
-r.post('/',async(req,res)=>res.json(await Menu.create(req.body)));
-export default r;
